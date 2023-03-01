fa2e1231f5589b18b3cd2c6f1e3825aaba5bef0d
David Zambrano Corral <david._101@hotmail.com>
2023-02-28 19:46:55 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -176,7 +176,7 @@ ORDER BY num_visits DESC
 LIMIT 1;
 
 --Performances
-SELECT COUNT(*) FROM visits where animal_id = 4;
-SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
